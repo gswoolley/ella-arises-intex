5b62f360cf2b1b84806a638a2c5e603fbb96a23e
Garett <g.woolley02@gmail.com>
2025-12-01 10:36:36 -0700

    Added app skeleton, views, layout, and ping route

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "layout");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/home"));
+app.use("/", require("./routes/ping"));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
