05a3ed82226f27812fc8b3fcdc0458c6bc15164d
Dawson Pitcher <dawsonlpitcher@gmail.com>
2025-12-03 18:29:48 -0700

    Restyle the admin/app/home-dashboard.ejs view

--- controller/admin/app/homeController.js
+++ controller/admin/app/homeController.js
@@ -1,5 +1,17 @@
 const getAdminHome = (req, res) => {
-  res.render('admin/app/home', { user: req.session.user });
+  const hour = new Date().getHours();
+  let greeting = 'Good afternoon';
+
+  if (hour < 12) {
+    greeting = 'Good morning';
+  } else if (hour >= 18) {
+    greeting = 'Good evening';
+  }
+
+  res.render('admin/app/home-dashboard', {
+    user: req.session.user,
+    greeting,
+  });
 };
 
 module.exports = {
